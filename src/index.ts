9af9ed1ccab30de222df93cb1000cc7c71beaf9a
bhanolia <lordzk06@gmail.com>
2023-06-28 12:49:22 +0700

    Mongo Setup :rocket:

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import compression from "compression";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose, { Error } from "mongoose";
 
 const app = express();
+dotenv.config();
 
 app.use(cors({ credentials: true }));
 app.use(compression());
@@ -16,4 +19,11 @@ const server = http.createServer(app);
 
 server.listen(8081, () => {
   console.log("Server Run On http://localhost:8081/");
+});
+
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO);
+mongoose.connection.on('error', (error:Error) => {
+  console.log(error);
 });
