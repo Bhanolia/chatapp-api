653e48eb239a2a5d7afeaa03dc7622951a8e4274
bhanolia <lordzk06@gmail.com>
2023-06-28 11:20:07 +0700

    Express Setup :rocket:

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Server Start");
+import express from "express";
+import http from "http";
+import compression from "compression";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8081, () => {
+  console.log("Server Run On http://localhost:8081/");
+});
